840d938d9896ecccc167b9c96b40d7f7bb5f4964
Gulfani Putra <gulfaniputra@gmail.com>
2024-05-05 14:34:27 +0800

    Create 'BudgetCard' custom component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css';
 import { Button, Container, Stack } from 'react-bootstrap';
+import BudgetCard from './components/BudgetCard';
 
 function App() {
   return (
     <Container className="my-4">
       <Stack direction="horizontal" gap="2" className="mb-4">
-        <h1 className="me-auto">Plutus</h1>
+        <h1 className="me-auto">Plutus App</h1>
         <Button variant="success">Add Budget</Button>
         <Button variant="outline-success">Add Expense</Button>
       </Stack>
@@ -15,7 +16,9 @@ function App() {
           gridTemplateColumns: 'repeat(auto-fill,minmax(300px, 1fr))',
           gap: '1rem',
           alignItems: 'flex-start',
-        }}></div>
+        }}>
+        <BudgetCard name="Entertainment" amount={200} max={1000}></BudgetCard>
+      </div>
     </Container>
   );
 }
